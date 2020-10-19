4983473ebeaa4d243e097c48047c628c9ca4444a
Sone4ka34 <68588587+Sone4ka34@users.noreply.github.com>
2020-10-19 18:40:00 +0300

    server_PUG

---
+++ server.js
@@ -0,0 +1,23 @@
+// const express = require("express");
+// // пакет node для работы с путями файлов
+// const path = require('path');
+
+// const port = process.env.PORT || 3000;
+// // msql 3306
+
+// const app = express();
+// // создание объекта приложения
+
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+
+// app.listen(port, () => {
+//   console.log(process)
+//   console.log(__dirname)
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
